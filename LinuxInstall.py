930af414f6dda946913bd93e3563ff11ea7826f1
Christian Hiland <hilandchristian112@gmail.com>
2023-09-08 17:28:21 +0000

    modified:   Install.json 	modified:   LinuxInstall.py

--- LinuxInstall.py
+++ LinuxInstall.py
@@ -1,23 +1,26 @@
 #/usr/bin/env python
 
 # Importing Modules
-import os
+import json
 import sys
+import os
 
+# Vars
+InstallJSON = "Install.json"
 DeviceSpace = int(sys.argv[1])
 MinimalDisk = 100
 
 def BigDisk():
     print("Yay You have enough space")
-def SmallDisk():
-    print("ERROR: You do not have enough disk space to install anything.")
 def SameDisk():
     print("WARNING: The install may not install all the way!")
+def SmallDisk():
+    print("ERROR: You do not have enough disk space to install anything.")
 
 if DeviceSpace > MinimalDisk:
     BigDisk()
-elif DeviceSpace < MinimalDisk:
-    SmallDisk()
 elif DeviceSpace == MinimalDisk:
+    SameDisk()
+elif DeviceSpace < MinimalDisk:
     SmallDisk()
     
